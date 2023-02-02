d799090260049cd4256c8063c6df2c44c7f3a648
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-02-02 17:14:45 +0900

    back

--- project/front/src/views/components/PopupDom.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from "react-dom";
-
-const PopupDom = ({ children }) => {
-  const a = document.getElementById("popupDom");
-  return ReactDom.createPortal(children, a);
-};
-
-export default PopupDom;
