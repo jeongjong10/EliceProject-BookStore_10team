d657a6d466602a766f6e0ee9466a0a9ed49dbffd
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-02-03 01:58:45 +0900

    detail, cart 기능개발 ing

--- project/front/src/views/routes/Detail.js
+++ project/front/src/views/routes/Detail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button, Container, Row, Col, Form } from "react-bootstrap";
 
@@ -10,6 +10,41 @@ import { item } from "../../temp"; // 상품 임시 데이터
 const Detail = () => {
   const { id } = useParams();
   const selected = item.find((x) => x.itemId == id);
+
+  const [count, setCount] = useState(1);
+
+  function getCount(e) {
+    setCount(Number(e.target.value));
+  }
+
+  function addCart() {
+    if (!localStorage.key("cart")) {
+      localStorage.setItem(
+        "cart",
+        JSON.stringify([
+          {
+            itemId: id,
+            count,
+          },
+        ])
+      );
+    } else {
+      const currentItems = JSON.parse(localStorage.getItem("cart"));
+      let currentItemsSet = new Set(currentItems);
+      currentItemsSet = [...currentItemsSet]; // ! 배열 해줄 필요 없다고 하셨는데 객체 상태에서 id값 어떻게 접근하는 지 모르겠음
+
+      // findIndex 돌 때 비교 조건값인 '-1' : 실제 id값은 랜덤값인데 -1로 해도 괜찮나?
+      if (currentItemsSet.findIndex((f) => f.itemId == id) != -1) {
+        alert("이미 장바구니에 있는 상품이네요!");
+      } else {
+        currentItemsSet.push({
+          itemId: id,
+          count,
+        });
+        localStorage.setItem("cart", JSON.stringify(currentItemsSet));
+      }
+    }
+  }
 
   return (
     <>
@@ -28,12 +63,17 @@ const Detail = () => {
               <Form>
                 <Form.Group className="mb-1">
                   <Form.Label>수량</Form.Label>
-                  <Form.Control type="count" />
+                  <Form.Control
+                    type="number"
+                    value={count}
+                    onChange={getCount}
+                  />
                 </Form.Group>
               </Form>
             </div>
             <div>
-              <Button>장바구니 추가</Button> <Button>바로 구매</Button>
+              <Button onClick={addCart}>장바구니 추가</Button>{" "}
+              <Button>바로 구매</Button>
             </div>
           </Col>
         </Row>
