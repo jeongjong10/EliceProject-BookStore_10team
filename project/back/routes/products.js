c087085edaa4696a42e22f3647d71a2169907f5c
ag-su <eunsugim815@gmail.com>
2023-02-03 20:34:33 +0900

    objectId 수정

--- project/back/routes/products.js
+++ project/back/routes/products.js
@@ -7,22 +7,20 @@ const mongoose = require("mongoose");
 router.get("/", async(req, res, next) => {
     try {
         // ------ 쿼리 스트링 ------
-        const idList = req.query["_id"];
-        let products;
-        if (typeof idList !== "object") idList = [idList];
-        else if (idList) {
-            products = await Promise.all(
-                idList.map(async(_id) => {
-                    const product = await Product.findOne({
-                        _id: mongoose.Types.ObjectId(_id),
-                    });
-                    return product;
-                })
-            );
-        } else {
-            products = await Product.find({});
-        }
-
+        // const idList = req.query["_id"];
+        // let products;
+        // if (typeof idList !== "object") idList = [idList];
+        // else if (idList) {
+        //     products = await Promise.all(
+        //         idList.map(async(_id) => {
+        //             const product = await Product.findOne({
+        //                 _id: mongoose.Types.ObjectId(_id),
+        //             });
+        //         })
+        //     );
+        // } else {
+        const products = await Product.find({});
+        // }
         res.json(products);
     } catch (e) {
         next(e);
@@ -36,7 +34,9 @@ router.get("/:_id", async(req, res, next) => {
 
         console.log(_id);
 
-        const product = await Product.findOne({ _id });
+        const id = mongoose.Types.ObjectId(_id);
+
+        const product = await Product.findOne({ id });
 
         if (!product) {
             console.error("존재하지 않는 상품입니다.");
