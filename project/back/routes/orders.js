2fd85464416e0d560481203354ccd5f29a1fcbeb
ag-su <eunsugim815@gmail.com>
2023-02-04 09:10:02 +0900

    [KES] order API에 verifyUser 기능 추가

--- project/back/routes/orders.js
+++ project/back/routes/orders.js
@@ -2,15 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { Order } = require("../models/index");
 
-// 현재 유저 불러오는 로직 추가
-// verifyUser => _id(유저), admin 여부
-
 // ------ USER: 현재 유저의 주문내역 조회 ------
 router.get("/", async(req, res, next) => {
     try {
-        // (예정) 현재 유저 불러오기
+        // 현재 유저 불러오기
+        const verifiedUser_id = await verifyUser(req.headers);
 
-        const orders = await Product.find({}); // (예정) 현재 유저의 주문내역 찾기
+        const orders = await Product.find({ userId: verifiedUser_id }); // 현재 유저의 주문내역 찾기
         res.json(orders);
         res.status(201).send({ message: "주문내역 조회" });
     } catch (e) {
@@ -21,8 +19,8 @@ router.get("/", async(req, res, next) => {
 // ------ USER: 현재 유저의 주문내역 저장 ------
 router.post("/", async(req, res, next) => {
     try {
-        // (예정) 현재 유저 불러오기
-        // const {_id} = verifyUser()
+        // 현재 유저 불러오기
+        const verifiedUser_id = await verifyUser(req.headers);
 
         // req.body: address(postalCode, address1, address2, recieverName, recieverPhone), orderNumber, comment, status, orderList(productName, count), totalProductPrice, shipping, totalPrice
         const orders = req.body;
@@ -30,7 +28,7 @@ router.post("/", async(req, res, next) => {
         // userId는 직접 추가
         // User의 userId와 혼동이 올 수 있음 (쥬문의 userId에는 User의 _id 값이 들어가기 때문 )
 
-        await Order.create({...orders, userId: _id });
+        await Order.create({...orders, userId: verifiedUser_id });
         res.status(201).send({ message: "주문내역 저장 성공" });
     } catch (e) {
         next(e);
@@ -38,16 +36,16 @@ router.post("/", async(req, res, next) => {
 });
 
 // ------ USER: 현재 유저의 주문내역 수정 ------
-router.post("/", async(req, res, next) => {
+router.post("/:_id", async(req, res, next) => {
     try {
-        // (예정) 현재 유저 불러오기
-        // const {_id} = verifyUser()
+        // 현재 유저 불러오기
+        const verifiedUser_id = await verifyUser(req.headers);
 
         const { _id } = req.params;
 
         const updateOrder = req.body;
 
-        await Order.findOneAndUpdate({ _id }, { updateOrder });
+        await Order.findOneAndUpdate({ _id, userId: verifiedUser_id }, { updateOrder });
         res.status(201).send({ message: "주문내역 수정 성공" });
     } catch (e) {
         next(e);
@@ -57,12 +55,12 @@ router.post("/", async(req, res, next) => {
 // ------ USER: 현재 유저의 주문내역 삭제 (비활성화) ------
 router.patch("/", async(req, res, next) => {
     try {
-        // (예정) 현재 유저 불러오기
-        // const {_id} = verifyUser()
+        // 현재 유저 불러오기
+        const verifiedUser_id = await verifyUser(req.headers);
 
         const { _id } = req.params;
 
-        await Order.findOneAndUpdate({ _id }, { activate: false });
+        await Order.findOneAndUpdate({ _id, userId: verifiedUser_id }, { activate: false });
         res.status(201).send({ message: "주문내역 비활성화 성공" });
     } catch (e) {
         next(e);
