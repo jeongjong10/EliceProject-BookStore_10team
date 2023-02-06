2dec407f0fc3ef149e893b523e6bd118c47d0c0d
Moon <anstjrdudwns88@gmail.com>
2023-02-06 21:21:13 +0900

    orderList해결

--- project/front/src/views/components/OrderProduct.js
+++ project/front/src/views/components/OrderProduct.js
@@ -1,10 +1,10 @@
 export const OrderProduct = (orders) => {
-  if (orders.orderList.legnth > 1) {
+  if (orders.orderList.length > 1) {
     return orders.orderList.map(
       (orderList, index) =>
-        `${orders.orderList[index].productName} / ${orders.orderList[index].count} 개`
+        `"${orders.orderList[index].productName}" : ${orders.orderList[index].count} 개  `
     );
   } else {
-    return `${orders.orderList[0].productName} / ${orders.orderList[0].count} 개`;
+    return `"${orders.orderList[0].productName}" : ${orders.orderList[0].count} 개`;
   }
 };
