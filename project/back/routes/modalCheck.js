78467d358978f48f976b8b13fd28b62302a72cb6
ag-su <eunsugim815@gmail.com>
2023-02-09 09:34:42 +0900

    [KES] modelCheck 병합 충돌 해결

--- project/back/routes/modalCheck.js
+++ project/back/routes/modalCheck.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const verifyUser = require("../middleware/verifyUser_middlewaring");
-const getHash = require("../utils/hash-password")
-const {User} = require("../models/index")
+const getHash = require("../utils/hash-password");
+const { User } = require("../models/index");
 
-router.post("/", verifyUser(), async (req, res, next) => {
-    console.log("------------------- 관리페이지 모달 PW 검사 요청 ------------------------");
+router.post("/", verifyUser(), async(req, res, next) => {
+    console.log(
+        "------------------- 관리페이지 모달 PW 검사 요청 ------------------------"
+    );
     try {
         // 비밀번호 입력
         const { password } = req.body;
@@ -21,18 +23,20 @@ router.post("/", verifyUser(), async (req, res, next) => {
             throw new Error("사용자의 정보가 없습니다");
         } else if (user.password !== getHash(password)) {
             console.error("사용자 입력 패스워드가 일치하지 않습니다");
-            console.log("------------------- 관리페이지 모달 PW 검사 실패 ------------------------");
+            console.log(
+                "------------------- 관리페이지 모달 PW 검사 실패 ------------------------"
+            );
             throw new Error("사용자 입력 패스워드가 일치하지 않습니다");
         } else {
             console.log(
                 "------------------- 비밀번호 일치 확인! ------------------------"
             );
         }
-        
-        res.status(200).end()
-    } catch(err) {
+
+        res.status(200).end();
+    } catch (err) {
         next(err);
     }
-})
+});
 
 module.exports = router;
