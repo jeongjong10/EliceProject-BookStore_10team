48d8b8897b68b53bd88f0c2631e3967633fe090b
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-02-09 12:02:25 +0900

    마이페이지-회원정보수정,회원탈퇴 css 완료

--- project/front/src/views/routes/Secession.js
+++ project/front/src/views/routes/Secession.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { customAxios } from "../../config/customAxios";
 import { useNavigate } from "react-router-dom";
-import { Container, Row, Col, Stack } from "react-bootstrap";
+import { Container, Row, Col, Nav, Button, Form } from "react-bootstrap";
 import AccountPrivacyModal from "../components/AccountPrivacyModal";
+import cssList from "../css/List.module.css";
+import cssSecession from "../css/Secession.module.css";
 
 const Secession = () => {
   const [modal, setModal] = useState(false);
@@ -21,13 +23,12 @@ const Secession = () => {
           password,
         },
       })
-
       .then((response) => {
         if (response.data.message === "비밀번호가 일치하지 않음") {
           alert("비밀번호가 일치하지 않습니다.");
           window.location.reload();
         } else {
-          alert("회원 탈퇴가 되었습니다.");
+          alert("회원 탈퇴 되었습니다. 다음에 다시 만나요! 👋");
           localStorage.removeItem("JWT");
           navigate("/");
         }
@@ -39,63 +40,57 @@ const Secession = () => {
       <Container className="subContainer">
         <Row>
           <Col xs lg="2">
-            <Stack gap={3}>
-              <button
-                className="order"
-                onClick={() => {
-                  navigate("/account/orders");
-                }}
-              >
-                주문조회
-              </button>
+            <Nav className="flex-column">
+              <Nav.Item className={cssList.unSelected}>
+                <a
+                  onClick={() => {
+                    navigate("/account/orders");
+                  }}
+                >
+                  주문 조회
+                </a>
+              </Nav.Item>
+              <Nav.Item className={cssList.unSelected}>
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setModal(true);
+                  }}
+                >
+                  회원정보 관리
+                </a>
+              </Nav.Item>
+              <Nav.Item className={cssList.selected}>
+                <a>회원탈퇴</a>
+              </Nav.Item>
               <AccountPrivacyModal
                 show={modal}
                 onHide={() => {
                   setModal(false);
                 }}
               />
-              <button
-                className="manager"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setModal(true);
-                }}
-              >
-                회원정보관리
-              </button>
-              <button className="deleted">회원탈퇴</button>
-            </Stack>
+            </Nav>
           </Col>
           <Col>
-            <h1>회원탈퇴</h1>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "80vh",
-              }}
-            >
-              <form style={{ display: "flex", flexDirection: "column" }}>
-                <h3>비밀번호</h3>
-                <input
+            <div className={cssSecession.alignCenter}>
+              <h2>정말 회원탈퇴 하시겠어요? 😥</h2>
+              <h5>탈퇴를 원하시면 비밀번호를 입력해 주세요.</h5>
+              <div className={cssSecession.form}>
+                <Form.Control
                   type="password"
                   value={password}
                   placeholder="******"
                   onChange={onPasswordHandler}
                 />
-                <div>
-                  <button
-                    onClick={(event) => {
-                      onClickConfirmButton(event);
-                    }}
-                  >
-                    확인
-                  </button>
-                </div>
-                <br />
-              </form>
+                <Button
+                  variant="dark"
+                  onClick={(event) => {
+                    onClickConfirmButton(event);
+                  }}
+                >
+                  탈퇴
+                </Button>
+              </div>
             </div>
           </Col>
         </Row>
